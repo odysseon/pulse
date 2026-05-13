c5f9f127e1bbe9da26ddd0774b4f92afaa009a96
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-05-13 10:31:31 +0100

    chore: move email to env variable

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,25 @@
-import { PrismaClient, Role } from '../generated/prisma/client.js';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Argon2PasswordHasher } from '@odysseon/whoami-adapter-argon2';
-import 'dotenv/config';
+import { PrismaClient, Role } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Argon2PasswordHasher } from "@odysseon/whoami-adapter-argon2";
+import "dotenv/config";
 
 // Create and configure the PrismaClient instance
 const adapter = new PrismaPg({
-  connectionString: process.env['DATABASE_URL'],
+  connectionString: process.env["DATABASE_URL"],
 });
 
 export const prisma = new PrismaClient({ adapter });
 const hasher = new Argon2PasswordHasher();
-const password = process.env['ADMIN_PASSWORD'];
+const password = process.env["ADMIN_PASSWORD"];
+const email = process.env["ADMIN_EMAIL"];
 
 async function main() {
-  if (!password) {
-    throw new Error('ADMIN_PASSWORD environment variable is not set');
+  if (!password || !email) {
+    throw new Error("ADMIN_PASSWORD environment variable is not set");
   }
-  const email = 'aanusteven8@gmail.com';
   const hashedPassword = await hasher.hash(password);
 
-  console.log('🌱 Seeding Admin Account and User profile...');
+  console.log("🌱 Seeding Admin Account and User profile...");
 
   const account = await prisma.account.upsert({
     where: { email },
@@ -33,9 +33,9 @@ async function main() {
       },
       user: {
         create: {
-          name: 'Steven Aanu',
+          name: "Steven Aanu",
           role: [Role.ADMIN, Role.USER],
-          avatarUrl: 'https://api.dicebear.com/7.x/avataaars/svg?seed=Steven',
+          avatarUrl: "https://api.dicebear.com/7.x/avataaars/svg?seed=Steven",
         },
       },
     },
