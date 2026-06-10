80e380f345b31518aa24bbe7d6327c3429e8b34b
Hammed Anuoluwapo Pelumi <stationphast@gmail.com>
2026-06-10 18:31:06 +0100

    refactor(mail): extract mail adapter interface for better abstraction
    - Create `MailAdapter` abstract class with `sendMail` method signature
    - Move nodemailer implementation to `NodemailerAdapter` implementing the interface
    - Simplify `MailService` to delegate to injected adapter (DIP compliance)
    - Register `NodemailerAdapter` as provider for `MailAdapter` token in module

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MailAdapter } from './mail.adapter.js';
+import { NodemailerAdapter } from './adapters/nodemailer.adapter.js';
 import { MailService } from './mail.service.js';
 
 @Module({
   imports: [ConfigModule],
-  providers: [MailService],
-  exports: [MailService],
+  providers: [
+    {
+      provide: MailAdapter,
+      useClass: NodemailerAdapter,
+    },
+    MailService,
+  ],
+  exports: [MailService, MailAdapter],
 })
 export class MailModule {}
